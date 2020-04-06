a2db5280508bababb556661e743495ec8c7cbaae
GiotaKyr <panagiota.k.kyriakou@gmaail.com>
2020-04-05 19:12:50 -0500

    Finished Python Intro

---
+++ scripts/My First Notebook.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Data Science Experience
+
+# **Panagiota Kyriakou**  
+# R&D Engineer
+
+# _I am interested in data science because I want to analyse and interper large amounts of data towards answering research questions._
+
+# ### The code in the next cell is supposed to do a simple calculation, i.e. 1+1=2
+
+# In[8]:
+
+
+1+1
+
+
+# #### Here is a list:
+# ***
+# 1. First `highlighted` Item
+# 2. Second Item
+# 3. Third Item  
+# 3.1 ~~And~~ a sub item 
+# > Life is good!
